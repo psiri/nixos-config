1254629d4c01b5cd0899346919bce4569f0e2c3a
psiri <paulsiri1@gmail.com>
2024-01-08 15:53:18 -0800

    Update configuration.nix

--- standard/nixos/configuration.nix
+++ standard/nixos/configuration.nix
@@ -21,7 +21,7 @@
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
-    ./hardware-configuration.nix
+    /etc/nixos/hardware-configuration.nix
   ];
 
   nixpkgs = {
