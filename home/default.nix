1b8cc45f35d2b5cd3decf55fe0668d25203220e8
psiri <paulsiri1@gmail.com>
2024-01-10 07:15:30 -0800

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -24,12 +24,12 @@
 
     nixpkgs = {
       # You can add overlays here
-      overlays = [
-        # Add overlays your own flake exports (from overlays and pkgs dir):
-        outputs.overlays.additions
-        outputs.overlays.modifications
-        outputs.overlays.unstable-packages
-      ];
+      # overlays = [
+      #   # Add overlays your own flake exports (from overlays and pkgs dir):
+      #   outputs.overlays.additions
+      #   outputs.overlays.modifications
+      #   outputs.overlays.unstable-packages
+      # ];
       # Configure your nixpkgs instance
       config = {
         # Disable if you don't want unfree packages
