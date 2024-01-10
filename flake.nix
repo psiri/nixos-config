67bc1f165a02bffb0f72af0128b9f52672fc0352
psiri <paulsiri1@gmail.com>
2024-01-10 06:17:11 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -52,10 +52,10 @@
     overlays = import ./overlays {inherit inputs;};
     # Reusable nixos modules you might want to export
     # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./modules/nixos;
+    #nixosModules = import ./modules/nixos;
     # Reusable home-manager modules you might want to export
     # These are usually stuff you would upstream into home-manager
-    homeManagerModules = import ./home;
+    #homeManagerModules = import ./home;
 
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
