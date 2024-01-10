92904ce22372db2f1c093dbae8c6bb5ab9cc53a9
psiri <paulsiri1@gmail.com>
2024-01-10 07:17:49 -0800

    Update standard.nix

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -28,9 +28,9 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+      # outputs.overlays.additions
+      # outputs.overlays.modifications
+      # outputs.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
