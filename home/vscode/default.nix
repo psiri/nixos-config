1447aae1dff38eaac61ed0e2e9d1ca2005ae1ffc
psiri <paulsiri1@gmail.com>
2024-01-11 13:34:57 -0800

    add most extensions

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -8,9 +8,21 @@
     enable = true;
     enableExtensionUpdateCheck = true;
     enableUpdateCheck = true;
-    #package = "pkgs.vscode"; # "pkgs.vscodium"
+    #package = pkgs.vscode; # pkgs.vscodium
+    
     extensions = [
+      pkgs.vscode-extensions.bbenoist.nix
+      pkgs.vscode-extensions.equinusocio.vsc-material-theme
+      pkgs.vscode-extensions.equinusocio.vsc-material-theme-icons
       pkgs.vscode-extensions.hashicorp.terraform
+      pkgs.vscode-extensions.ms-azuretools.vscode-docker
+      pkgs.vscode-extensions.ms-python.python
+      pkgs.vscode-extensions.ms-python.vscode-pylance
+      pkgs.vscode-extensions.oderwat.indent-rainbow
+      pkgs.vscode-extensions.wholroyd.jinja
+      pkgs.vscode-extensions.yzhang.markdown-all-in-one
+      # TODO - find solution for installing missing extensions
+      # Checkov, Material Theme Icons,
     ];
     globalSnippets = {};
     haskell.enable = false;
