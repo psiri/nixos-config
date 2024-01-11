3a8081a8a76b4893c3166c8540f7f1480a83440e
psiri <paulsiri1@gmail.com>
2024-01-11 06:26:53 -0800

    Update standard.nix

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -165,14 +165,19 @@
     enableBashCompletion = true;
     enableCompletion = true;
     autosuggestions.enable = true;
-    initExtra = ''
-      [[ ! -f ${./p10k.zsh;} ]] || source ${./p10k.zsh}
-    '';
+    # initExtra = ''
+    #   [[ ! -f ${./p10k.zsh;} ]] || source ${./p10k.zsh}
+    # '';
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
       }
     ];
     syntaxHighlighting.enable = true;
