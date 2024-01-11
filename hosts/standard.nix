55de6f134cb012049232d28175097e60752fe8b1
psiri <paulsiri1@gmail.com>
2024-01-11 06:53:37 -0800

    Update standard.nix

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -165,6 +165,23 @@
     enableBashCompletion = true;
     enableCompletion = true;
     autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    syntaxHighlighting.highlighters = ["main" "brackets" "pattern" "cursor" "line"];
+    syntaxHighlighting.patterns = {};
+    syntaxHighlighting.styles = {"globbing" = "none";};
+    #promptInit = "info='n host cpu os wm sh n' fet.sh";
+    ohMyZsh = {
+      enable = true;
+      plugins = ["sudo" "terraform" "systemadmin" "vi-mode" "colorize" "powerlevel10k"];
+      theme = "powerlevel10k"; # robbyrussel jnrowe muse obraun ys
+    };
+  };
+
+  home-manager.users.${user}.programs.zsh = {
+    enable = true;
+    enableBashCompletion = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
     # initExtra = ''
     #   [[ ! -f ${./p10k.zsh;} ]] || source ${./p10k.zsh}
     # '';
