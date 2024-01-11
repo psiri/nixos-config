1bd21d5d2bbe6f5263fec8eb4433e47b2ae218e8
psiri <paulsiri1@gmail.com>
2024-01-11 12:30:10 -0800

    Update default.nix

--- home/flameshot/default.nix
+++ home/flameshot/default.nix
@@ -11,7 +11,7 @@
     enable = true;
     settings = {       # Example configuration.ini has be included here for reference. Also see (https://github.com/flameshot-org/flameshot/blob/master/flameshot.example.ini)
       General = {
-        savePath=/tmp; # Save to /tmp (which is RAM with our config) to ensure screenshots are non-persistent
+        savePath="/tmp"; # Save to /tmp (which is RAM with our config) to ensure screenshots are non-persistent
         saveAsFileExtension=".png";
         disabledTrayIcon=false;
         startupLaunch=true;
