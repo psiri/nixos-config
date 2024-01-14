e1fc1a748e87c0f1f4835cdbbbe80f8ac0fd99c9
psiri <paulsiri1@gmail.com>
2024-01-13 16:00:54 -0800

    collapse

--- home/swaylock/default.nix
+++ home/swaylock/default.nix
@@ -4,25 +4,26 @@
   user,
   ...
 }: {
-  home-manager.users.${user}.home.file.".config/hypr/per-app/swaylock.conf" = {
-    text = ''
-      bind = $mainMod, L, exec, swaylock -c ff000000
-    '';
-  };
-
   security.pam.services.swaylock = {
     text = ''
       auth include login
     '';
   };
 
-  home-manager.users.${user}.programs.swaylock = {
-    enable = true;
-    settings = {
-      font-size = 24;
-      indicator-idle-visible = false;
-      indicator-rafius = 100;
-      show-failed-attempts = true;
+  home-manager.users.${user} = {
+    programs.swaylock = {
+      enable = true;
+      settings = {
+        font-size = 24;
+        indicator-idle-visible = false;
+        indicator-rafius = 100;
+        show-failed-attempts = true;
+      };
+    };
+    home.file.".config/hypr/per-app/swaylock.conf" = {
+      text = ''
+        bind = $mainMod, L, exec, swaylock -c ff000000
+      '';
     };
   };
 }
