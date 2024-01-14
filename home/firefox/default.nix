64fc909ff3062a23e644ec21277cc42c7ee3cef1
psiri <paulsiri1@gmail.com>
2024-01-14 12:21:40 -0800

    retry

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -36,62 +36,77 @@
         #Exceptions = [];
       };
       ExtensionSettings = {
+        "*" = {
+          blocked_install_message = "This extension has been blocked by your organization.";
+          allowed_types = ["extension"];
+        };
         "uBlock@raymondhill.net" = {
           installation_mode = "force_installed";
           installation_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
         };
       };
+      FirefoxHome = {
+        Search = true;
+        Pocket = false;
+        Snippets = false;
+        TopSites = false;
+        Highlights = false;
+      };
       NoDefaultBookmarks = true;
       OfferToSaveLogins = false;
       OfferToSaveLoginsDefault = false;
       PasswordManagerEnabled = false;
-      #SSLVersionMin = "tls1.2";
-    };
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-      extraPolicies = {
-        CaptivePortal = false;
-        DisablePasswordReveal = true;
-        DisablePocket = true;
-        DisableFirefoxAccounts = false;
-        DisableFirefoxStudies = true;
-        DisableTelemetry = true;
-        DisplayBookmarksToolbar = "always";
-        DontCheckDefaultBrowser = true;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-          #Exceptions = [];
-        };
-        ExtensionSettings = {
-          "*" = {
-            blocked_install_message = "This extension has been blocked by your organization.";
-            allowed_types = ["extension"];
-          };
-          "uBlock@raymondhill.net" = {
-            installation_mode = "force_installed";
-            installation_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          };
-        };
-        NoDefaultBookmarks = true;
-        OfferToSaveLogins = false;
-        OfferToSaveLoginsDefault = false;
-        PasswordManagerEnabled = false;
-        FirefoxHome = {
-          Search = true;
-          Pocket = false;
-          Snippets = false;
-          TopSites = false;
-          Highlights = false;
-        };
-        UserMessaging = {
-          ExtensionRecommendations = false;
-          SkipOnboarding = true;
-        };
-        SSLVersionMin = "tls1.2";
+      SSLVersionMin = "tls1.2";
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        SkipOnboarding = true;
       };
     };
+    # package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+    #   extraPolicies = {
+    #     CaptivePortal = false;
+    #     DisablePasswordReveal = true;
+    #     DisablePocket = true;
+    #     DisableFirefoxAccounts = false;
+    #     DisableFirefoxStudies = true;
+    #     DisableTelemetry = true;
+    #     DisplayBookmarksToolbar = "always";
+    #     DontCheckDefaultBrowser = true;
+    #     EnableTrackingProtection = {
+    #       Value = true;
+    #       Locked = true;
+    #       Cryptomining = true;
+    #       Fingerprinting = true;
+    #       #Exceptions = [];
+    #     };
+    #     ExtensionSettings = {
+    #       "*" = {
+    #         blocked_install_message = "This extension has been blocked by your organization.";
+    #         allowed_types = ["extension"];
+    #       };
+    #       "uBlock@raymondhill.net" = {
+    #         installation_mode = "force_installed";
+    #         installation_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+    #       };
+    #     };
+    #     NoDefaultBookmarks = true;
+    #     OfferToSaveLogins = false;
+    #     OfferToSaveLoginsDefault = false;
+    #     PasswordManagerEnabled = false;
+    #     FirefoxHome = {
+    #       Search = true;
+    #       Pocket = false;
+    #       Snippets = false;
+    #       TopSites = false;
+    #       Highlights = false;
+    #     };
+    #     UserMessaging = {
+    #       ExtensionRecommendations = false;
+    #       SkipOnboarding = true;
+    #     };
+    #     SSLVersionMin = "tls1.2";
+    #   };
+    # };
     # policies # TODO - automatically install Firefox extensions
   };
 }
