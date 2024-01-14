f9c7005cc784d576b6a25419bef8f326ac472aad
psiri <paulsiri1@gmail.com>
2024-01-13 18:49:50 -0800

    wallpaper update

--- home/hypr/hyprpaper.nix
+++ home/hypr/hyprpaper.nix
@@ -5,12 +5,12 @@
 }: {
   home-manager.users.${user}.home.file.".config/hypr/hyprpaper.conf" = {
     text = ''
-      preload = ~/nixos/wallpaper/5.jpg
-      preload = ~/nixos/wallpaper/6.jpg
-      preload = ~/nixos/wallpaper/7.jpg
-      preload = ~/nixos/wallpaper/8.jpg
+      preload = ~/Pictures/Wallpaper/5.jpg
+      preload = ~/Pictures/Wallpaper/6.jpg
+      preload = ~/Pictures/Wallpaper/7.jpg
+      preload = ~/Pictures/Wallpaper/8.jpg
 
-      wallpaper = eDP-1, ~/nixos/wallpaper/5.jpg
+      wallpaper = eDP-1, ~/Pictures/Wallpaper/5.jpg
     '';
     # wallpaper = eDP-1, ~/nixos/wallpaper/5.jpg
     # this value needs to have a configuration for both laptop and desktop
