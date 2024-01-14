7d09b9d932f1c60a3bb23972432de18d2cc30a29
psiri <paulsiri1@gmail.com>
2024-01-14 08:11:42 -0800

    reduce height

--- home/waybar/config.jsonc.nix
+++ home/waybar/config.jsonc.nix
@@ -11,7 +11,7 @@
         "layer": "top",
         // "output": [],
         "position": "top",
-        "height": 20,    // blank = dynamic
+        "height": 16,    // blank = dynamic
         // "width": "",  // blank = dynamic
         // "margin": "",
         "margin-top": 2,
