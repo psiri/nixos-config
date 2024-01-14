a2d2b2d58ed5eb016216d358c3b7017202208824
psiri <paulsiri1@gmail.com>
2024-01-14 05:48:06 -0800

    add color config

--- home/swaylock/default.nix
+++ home/swaylock/default.nix
@@ -30,6 +30,37 @@
         datestr = "%a %e %B";
         effect-blur="5x2";
         image="~/Pictures/Wallpaper/4.jpg";
+        key-hl-color="00000066";
+        separator-color="00000000";
+
+        inside-color="00000033";
+        inside-clear-color="ffffff00";
+        inside-caps-lock-color="ffffff00";
+        inside-ver-color="ffffff00";
+        inside-wrong-color="ffffff00";
+
+        ring-color="ffffff";
+        ring-clear-color="ffffff";
+        ring-caps-lock-color="ffffff";
+        ring-ver-color="ffffff";
+        ring-wrong-color="ffffff";
+
+        line-color="00000000";
+        line-clear-color="ffffffFF";
+        line-caps-lock-color="ffffffFF";
+        line-ver-color="ffffffFF";
+        line-wrong-color="ffffffFF";
+
+        text-color="ffffff";
+        text-clear-color="ffffff";
+        text-ver-color="ffffff";
+        text-wrong-color="ffffff";
+
+        bs-hl-color="ffffff";
+        caps-lock-key-hl-color="ffffffFF";
+        caps-lock-bs-hl-color="ffffffFF";
+        text-caps-lock-color="ffffff";
+
       };
     };
     home.file.".config/hypr/per-app/swaylock.conf" = {
