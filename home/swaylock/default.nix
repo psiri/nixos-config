c3baf82e1869e5ccdf0c6f84f44f60c215b1f455
psiri <paulsiri1@gmail.com>
2024-01-15 19:01:47 -0800

    config. ?

--- home/swaylock/default.nix
+++ home/swaylock/default.nix
@@ -82,13 +82,13 @@
     services.swayidle = {
       enable = true;
       events = [
-        { event = "before-sleep"; command = "${home-manager.users.${user}.programs.swaylock.package}/bin/swaylock -f"; }
+        { event = "before-sleep"; command = "${config.home-manager.users.${user}.programs.swaylock.package}/bin/swaylock -f"; }
         { event = "lock"; command = "swaylock"; }
       ];
       timeouts = [
         {
           timeout = 20;
-          command = "${home-manager.users.${user}.programs.swaylock.package}/bin/swaylock -f";
+          command = "${config.home-manager.users.${user}.programs.swaylock.package}/bin/swaylock -f";
         }
         # { 
         #   timeout = 180;
