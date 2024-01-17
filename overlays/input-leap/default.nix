bc95f44c8f6d2b2f45962d5736fa17ed7052c5da
psiri <paulsiri1@gmail.com>
2024-01-17 08:41:46 -0800

    retry

--- overlays/input-leap/default.nix
+++ overlays/input-leap/default.nix
@@ -1,6 +1,6 @@
-(self: super: {
+self: super: {
   # Customized input-leap package
-  input-leap = super.input-leap.override {
+  input-leap = super.input-leap.override (old: rec {
     version = "unstable-2023-12-27";
     src = fetchFromGitHub {
       owner = "input-leap";
@@ -9,7 +9,7 @@
       hash = "sha256-TEv1xR1wUG3wXNATLLIZKOtW05X96wsPNOlE77OQK54=";
       fetchSubmodules = true;
     };
-  };
+  });
   #input-leap = builtins.fetchTarball https://github.com/input-leap/input-leap/archive/refs/tags/v2.4.0.tar.gz
   #"https://github.com/input-leap/input-leap/releases/download/1.30.0/dhall-1.30.0-x86_64-linux.tar.bz2";
-})
+}
