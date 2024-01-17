ad6502099fcf29f4ec5b29bdb55f527549167831
psiri <paulsiri1@gmail.com>
2024-01-17 07:02:12 -0800

    retry

--- home/input-leap/default.nix
+++ home/input-leap/default.nix
@@ -7,16 +7,48 @@
   ...
 }: 
 
+let
+  nixos-unstable = import <nixos-unstable> {
+    # Include the nixos config when importing nixos-unstable
+    # But remove packageOverrides to avoid infinite recursion
+    config = removeAttrs config.nixpkgs.config [ "packageOverrides" ];
+  };
+in
+{
+  disabledModules = [ "programs/input-leap.nix" ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Override the input-leap module
+      <nixos-unstable/nixos/modules/programs/input-leap.nix>
+    ];
+
+  # Override select packages to use the unstable channel
+  nixpkgs.config.packageOverrides = pkgs: {
+    input-leap = nixos-unstable.input-leap;
+  };
+
+  users.users.${user}.packages = [ pkgs.input-leap ];
+}
+
+
+
+################### OPT 1
+
 # let
 #   nixpkgs-unstable-input-leap = builtins.fetchurl "https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/applications/misc/input-leap/default.nix";
 #   #builtins.fetchurl "https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/misc/input-leap";
 # in
 
-{
-  users.users.${user}.packages = [ pkgs.input-leap ];
-  #imports = [ "${nixpkgs-unstable-input-leap}" ];
-}
 
+
+
+
+
+
+
+
+################# OPT 2
 
 #   let
 #     fetch-unstable = pkg: pkg.override (origArgs: builtins.intersectAttrs origArgs self);
