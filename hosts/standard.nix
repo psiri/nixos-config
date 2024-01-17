ac36ab8a1bec1dc900a6f3df982e68e619795306
psiri <paulsiri1@gmail.com>
2024-01-17 08:27:15 -0800

    retry split-out

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -39,20 +39,20 @@
       # outputs.overlays.unstable-packages
 
 
-      #../overlays/input-leap/default.nix
+      ../overlays/input-leap/default.nix
 
-      (self: super: {
-        # Customized input-leap package
-        input-leap = super.input-leap.override {
-          src = fetchFromGitHub {
-            owner = "input-leap";
-            repo = "input-leap";
-            rev = "ecf1fb6645af7b79e6ea984d3c9698ca0ab6f391";
-            hash = "sha256-TEv1xR1wUG3wXNATLLIZKOtW05X96wsPNOlE77OQK54=";
-            fetchSubmodules = true;
-          };
-        };
-      })
+      # (self: super: {
+      #   # Customized input-leap package
+      #   input-leap = super.input-leap.override {
+      #     src = fetchFromGitHub {
+      #       owner = "input-leap";
+      #       repo = "input-leap";
+      #       rev = "ecf1fb6645af7b79e6ea984d3c9698ca0ab6f391";
+      #       hash = "sha256-TEv1xR1wUG3wXNATLLIZKOtW05X96wsPNOlE77OQK54=";
+      #       fetchSubmodules = true;
+      #     };
+      #   };
+      # })
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
--- overlays/input-leap/default.nix
+++ overlays/input-leap/default.nix
@@ -8,7 +8,7 @@
       hash = "sha256-TEv1xR1wUG3wXNATLLIZKOtW05X96wsPNOlE77OQK54=";
       fetchSubmodules = true;
     };
-  }
+  };
   #input-leap = builtins.fetchTarball https://github.com/input-leap/input-leap/archive/refs/tags/v2.4.0.tar.gz
   #"https://github.com/input-leap/input-leap/releases/download/1.30.0/dhall-1.30.0-x86_64-linux.tar.bz2";
 })
