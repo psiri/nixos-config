00ddfc540dbea5028d06d19d739e2f52ed74704e
psiri <paulsiri1@gmail.com>
2024-01-17 06:20:43 -0800

    try alternative

--- home/input-leap/default.nix
+++ home/input-leap/default.nix
@@ -2,12 +2,28 @@
   config,
   inputs,
   outputs,
-  pkgs,
   user,
+  pkgs,
   ...
-}: {
-  
-  users.users.${user}.packages = with pkgs; [
-    ${nixpkgs-unstable}.input-leap
-  ];
+}: 
+
+let
+  nixpkgs-unstable-input-leap = builtins.fetchurl "https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/misc/input-leap";
+in
+
+{
+  imports = [ "${nixpkgs-unstable-input-leap}" ];
 }
+
+
+#   let
+#     fetch-unstable = pkg: pkg.override (origArgs: builtins.intersectAttrs origArgs self);
+#     nixpkgs-unstable = import <nixpkgs-unstable> {};
+#   in
+
+# {
+#   input-leap-unstable = assert (! super ? input-leap-unstable); (cherryPick nixpkgs-unstable.input-leap).override {
+#     llvmPackages = self.llvmPackages_11;
+#   };
+# }
+
