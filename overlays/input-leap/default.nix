5a2569cd8fa3a49dbde761d494ee96573a6a4c32
psiri <paulsiri1@gmail.com>
2024-01-17 09:15:10 -0800

    attrs

--- overlays/input-leap/default.nix
+++ overlays/input-leap/default.nix
@@ -1,7 +1,7 @@
 
 (self: super: {
   # Customized input-leap package
-  input-leap = super.input-leap.override (old: {
+  input-leap = super.input-leap.overrideAttrs (old: {
     #version = "unstable-2023-12-27";
     src = super.fetchFromGitHub {
       owner = "input-leap";
