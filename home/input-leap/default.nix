c1ff5d51dbd5979795d7ee25a3b2eafd6d3ca3c5
psiri <paulsiri1@gmail.com>
2024-01-17 06:32:39 -0800

    with tree

--- home/input-leap/default.nix
+++ home/input-leap/default.nix
@@ -8,7 +8,7 @@
 }: 
 
 let
-  nixpkgs-unstable-input-leap = builtins.fetchurl "https://github.com/NixOS/nixpkgs/nixos-unstable/pkgs/applications/misc/input-leap";
+  nixpkgs-unstable-input-leap = builtins.fetchurl "https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/applications/misc/input-leap";
   #builtins.fetchurl "https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/misc/input-leap";
 in
 
