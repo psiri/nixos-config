45b8812278a1437b331aeddd577938143d766cdb
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:41:33 -0800

    fix sacale

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -2,7 +2,7 @@
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
       # Desktop left monitor
-      monitor=DP-1,3840x2160@60,auto,1,transform,1.5
+      monitor=DP-1,3840x2160@60,auto,1.5,transform,1
       # Desktop middle monitor
       monitor=DP-2,3840x2160@60,auto,1.5
       # Built-in display
