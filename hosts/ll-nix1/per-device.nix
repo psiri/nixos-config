09503e69d47466e740c52985b8ffc4d9fd785f13
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:28:23 -0800

    try preferred

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -6,7 +6,7 @@
       # Desktop left monitor
       monitor=DP-1,3840x2160,auto,1,transform,1
       # Desktop middle monitor
-      monitor=DP-2,3840x2160,auto,1
+      monitor=DP-2,preferred,auto,1
     '';
   };
 }
