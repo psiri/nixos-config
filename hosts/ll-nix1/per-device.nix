594c0fdd9166956a5afa8c313944f5faac38fe23
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:40:09 -0800

    set refresh

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -2,9 +2,9 @@
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
       # Desktop left monitor
-      monitor=DP-1,3840x2160,auto,1,transform,1.5
+      monitor=DP-1,3840x2160@60,auto,1,transform,1.5
       # Desktop middle monitor
-      monitor=DP-2,preferred,auto,1.5
+      monitor=DP-2,3840x2160@60,auto,1.5
       # Built-in display
       monitor=eDP-1,preferred,auto,1
     '';
