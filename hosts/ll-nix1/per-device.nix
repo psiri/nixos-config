5cfe494ff2ca00973940dc9745b21ef6301d2bf6
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:54:10 -0800

    try force 144

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -4,7 +4,7 @@
       # Desktop left monitor
       monitor=DP-1,3840x2160@60,auto,1.5,transform,1
       # Desktop middle monitor
-      monitor=DP-2,3840x2160@60,auto,1.5
+      monitor=DP-2,3840x2160@144,auto,1.5
       # Built-in display
       # monitor=eDP-1,preferred,auto,1
       monitor=eDP-1,disable
