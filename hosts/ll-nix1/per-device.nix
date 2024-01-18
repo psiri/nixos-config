fd03195f315a2e2112b9065223d5fdfb8086d1b1
Paul Siri <paulsiri1@gmail.com>
2024-01-18 13:02:15 -0800

    test monitor transform

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -1,7 +1,12 @@
 {user, ...}: {
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
-      monitor=,3840x2160,auto,auto
+      # Built-in display
+      monitor=eDP-1,1920x1080,auto,1
+      # Desktop left monitor
+      monitor=DP-1,3840x2160,auto,1,transform,1
+      # Desktop middle monitor
+      monitor=DP-2,3840x2160,auto,1
     '';
   };
 }
