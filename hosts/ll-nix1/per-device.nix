aaf413630dcc3a226df9375e615e78b86f4fa67e
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:29:45 -0800

    scale

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -1,12 +1,12 @@
 {user, ...}: {
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
+      # Desktop left monitor
+      monitor=DP-1,3840x2160,auto,1,transform,1.5
+      # Desktop middle monitor
+      monitor=DP-2,preferred,auto,1.5
       # Built-in display
       monitor=eDP-1,preferred,auto,1
-      # Desktop left monitor
-      monitor=DP-1,3840x2160,auto,1,transform,1
-      # Desktop middle monitor
-      monitor=DP-2,preferred,auto,1
     '';
   };
 }
