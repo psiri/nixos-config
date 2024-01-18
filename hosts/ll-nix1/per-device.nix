f0addaf7c0c160ba7397d2448a486b6b0a754ee1
Paul Siri <paulsiri1@gmail.com>
2024-01-18 14:59:00 -0800

    set to working config

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -4,10 +4,10 @@
       # Desktop left monitor
       monitor=DP-1,3840x2160@60,auto,1.5,transform,1
       # Desktop middle monitor
-      monitor=DP-2,3840x2160@144,auto,1.5
+      monitor=DP-2,3840x2160@60,auto,1.5
       # Built-in display
-      # monitor=eDP-1,preferred,auto,1
-      monitor=eDP-1,disable
+      monitor=eDP-1,preferred,auto,1
+      #monitor=eDP-1,disable
     '';
   };
 }
