bd3e4214d4cb4fe538aa567868c0f7fe1862e63b
Paul Siri <paulsiri1@gmail.com>
2024-01-19 12:35:38 -0800

    split settings file

--- home/ulauncher/default.nix
+++ home/ulauncher/default.nix
@@ -16,26 +16,10 @@
     '';
   };
 
-  home-manager.users.${user}.home.file.".config/ulauncher/settings.json" = {
-    text = ''
-      {
-        "blacklisted-desktop-dirs": "/usr/share/locale:/usr/share/app-install:/usr/share/kservices5:/usr/share/fk5:/usr/share/kservicetypes5:/usr/share/applications/screensavers:/usr/share/kde4:/usr/share/mimelnk",
-        "clear-previous-query": true,
-        "disable-desktop-filters": true,
-        "grab-mouse-pointer": true,
-        "hotkey-show-app": "<Super>b",
-        "render-on-screen": "mouse-pointer-monitor",
-        "show-indicator-icon": false,
-        "show-recent-apps": "0",
-        "terminal-command": "",
-        "theme-name": "TokyoNight-Theme"
-      }
-    '';
-  };
-
   imports = [
     ./manifest.json.nix
     ./theme-gtk-3.20.css.nix
     ./theme.css.nix
+    ./settings.json.nix
   ];
 }
