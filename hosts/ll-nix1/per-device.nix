8932d5b8c95e03b9f7e7c82b65c7aff68ee6d15e
Paul Siri <paulsiri1@gmail.com>
2024-01-19 07:44:45 -0800

    change monitor orders

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -1,11 +1,11 @@
 {user, ...}: {
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
-      # Desktop left monitor
-      monitor=DP-1,3840x2160@60,auto,1.5,transform,1
       # Desktop middle monitor
-      monitor=DP-2,3840x2160@60,auto,1.5
-      #monitor=DP-2,disable
+      monitor=DP-1,3840x2160@60,auto,1.5
+      # Desktop left monitor
+      monitor=DP-2,3840x2160@60,auto,1.5,transform,1
+      # monitor=DP-2,disable
       # Built-in display
       monitor=eDP-1,preferred,auto,1
       #monitor=eDP-1,disable
