d2e3a8f23a146187ba0d98e8cafdb3bf1c70ed6c
Paul Siri <paulsiri1@gmail.com>
2024-01-19 07:28:49 -0800

    re-enable monitor 2

--- hosts/ll-nix1/per-device.nix
+++ hosts/ll-nix1/per-device.nix
@@ -4,8 +4,8 @@
       # Desktop left monitor
       monitor=DP-1,3840x2160@60,auto,1.5,transform,1
       # Desktop middle monitor
-      # monitor=DP-2,3840x2160@60,auto,1.5
-      monitor=DP-2,disable
+      monitor=DP-2,3840x2160@60,auto,1.5
+      #monitor=DP-2,disable
       # Built-in display
       monitor=eDP-1,preferred,auto,1
       #monitor=eDP-1,disable
