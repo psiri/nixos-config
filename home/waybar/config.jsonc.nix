5c90974a0d26b3fdeb0bd19ab43a7f15443ecf44
Paul Siri <paulsiri1@gmail.com>
2024-01-20 19:16:48 -0800

    reformat

--- home/waybar/config.jsonc.nix
+++ home/waybar/config.jsonc.nix
@@ -97,7 +97,7 @@
           "interval": 1,
           "on-click": "",
           "tooltip": true,
-          "tooltip-format": "Overall: {total} GiB / {avail} GiB ({percentage}%)\n Swap: {swapUsed} GiB / {swapAvail} GiB ({swapPercentage}%)"
+          "tooltip-format": "Overall: {avail} GiB / {total} GiB ({percentage}%)\n Swap: {swapUsed} GiB / {swapAvail} GiB ({swapPercentage}%)"
         },
 
         "backlight": {
