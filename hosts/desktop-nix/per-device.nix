95f621e4302a57b48789d94b8130758783a5822c
Paul Siri <paulsiri1@gmail.com>
2024-01-22 12:28:06 -0800

    again

--- hosts/desktop-nix/per-device.nix
+++ hosts/desktop-nix/per-device.nix
@@ -1,8 +1,8 @@
 {user, ...}: {
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
-      monitor=DP-1,3840x2160,auto,1.5
-      monitor=DP-2,3840x2160,auto,1.5
+      monitor=,3840x2160,auto,1.5
+      monitor=DP-2,3840x2160,0x0,1.5
       monitor=DP-3,3840x2160,0x-3840,1.5,transform,1
     '';
   };
