a430772a9ff4c86be789acf806a01b43733cadd1
Paul Siri <paulsiri1@gmail.com>
2024-01-22 12:33:37 -0800

    again

--- hosts/desktop-nix/per-device.nix
+++ hosts/desktop-nix/per-device.nix
@@ -3,7 +3,7 @@
     text = ''
       monitor=,3840x2160,auto,1.5
       monitor=DP-2,3840x2160,0x0,1.5
-      monitor=DP-3,3840x2160,-2560x0,1.5,transform,1
+      monitor=DP-3,3840x2160,-1440x0,1.5,transform,1
     '';
   };
 }
