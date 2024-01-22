abee8e21547fcd1184c7bfba721b4546f4545030
Paul Siri <paulsiri1@gmail.com>
2024-01-22 12:16:38 -0800

    retry xform

--- hosts/desktop-nix/per-device.nix
+++ hosts/desktop-nix/per-device.nix
@@ -3,7 +3,7 @@
     text = ''
       monitor=DP-1,3840x2160,auto,1.5
       monitor=DP-2,3840x2160,auto,1.5
-      monitor=DP-3,3840x2160,-2560x0,1.5,transform,1
+      monitor=DP-3,3840x2160,0x-2560,1.5,transform,1
     '';
   };
 }
