a8c6c8cdb8a234f4ca0e865da8379f5d6522c59a
Paul Siri <paulsiri1@gmail.com>
2024-01-23 07:11:52 -0800

    try cifs mount

--- home/thunar/default.nix
+++ home/thunar/default.nix
@@ -25,4 +25,13 @@
   };
 
   networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
+  fileSystems."/mnt/share" = {
+    device = "//sv1-nas1";
+    fsType = "cifs";
+    # options = let
+    #   # this line prevents hanging on network split
+    #   automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    # in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
 }
