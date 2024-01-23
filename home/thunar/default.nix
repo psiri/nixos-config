58f46733635762f9833eebb75a44e285c979e7de
Paul Siri <paulsiri1@gmail.com>
2024-01-23 07:48:45 -0800

    re-enable

--- home/thunar/default.nix
+++ home/thunar/default.nix
@@ -7,22 +7,22 @@
   lib,
   ...
 }: {
-  # programs.thunar = {
-  #   enable = true;
-  #   plugins = with pkgs.xfce; [
-  #     thunar-archive-plugin
-  #     thunar-volman
-  #   ];
-  # };
-  # services = {
-  #   gvfs = {
-  #     enable = true; # Mount, trash, and other functionalities
-  #     package = lib.mkForce pkgs.gnome3.gvfs; # use the gnome versions to support smb:// in thunar
-  #   };
-  #   tumbler = {
-  #     enable = true; # Thumbnail support for images
-  #   };
-  # };
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
+  services = {
+    gvfs = {
+      enable = true; # Mount, trash, and other functionalities
+      package = lib.mkForce pkgs.gnome3.gvfs; # use the gnome versions to support smb:// in thunar
+    };
+    tumbler = {
+      enable = true; # Thumbnail support for images
+    };
+  };
 
   #networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
   # fileSystems."/mnt/nas" = {
