c44e5b9ed1b8fef3eb526212f69b1170888a0852
psiri <paulsiri1@gmail.com>
2024-01-24 08:18:50 -0800

    retry

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -190,8 +190,8 @@ bind = $mainMod, P, pseudo, dwindle
 bind = $mainMod SHIFT, J, togglesplit, # dwindle
 
 # Window Rules
-windowrulev2 = opacity 1.0 1.0 override,title:^(.*)[( - Netflix)|(Twitch — )|(\- YouTube -)](.*)$  # Disables opacity of any YouTube windows (regardless what browser they're running in)
-windowrulev2 = nodim,title:^(.*)[( - Netflix)|(Twitch — )|(\- YouTube -)](.*)$                     # Disables dimming of any YouTube windows (regardless what browser they're running in)
+windowrulev2 = opacity 1.0 1.0 override,title:^(.*)[( - Netflix)|(Twitch — )|( - YouTube -)](.*)$  # Disables opacity of any YouTube windows (regardless what browser they're running in)
+windowrulev2 = nodim,title:^(.*)[( - Netflix)|(Twitch — )|( - YouTube -)](.*)$                     # Disables dimming of any YouTube windows (regardless what browser they're running in)
 #windowrulev2 = opacity 1.0 1.0 override,title:^(.*)( - Netflix)(.*)$  # Disables opacity of any Netflix windows (regardless what browser they're running in)
 #windowrulev2 = nodim,title:^(.*)( - Netflix)(.*)$                     # Disables dimming of any Netflix windows (regardless what browser they're running in)
 #windowrulev2 = opacity 1.0 1.0 override,title:^(.*)( Twitch - )(.*)$  # Disables opacity of any Twitch windows (regardless what browser they're running in)
