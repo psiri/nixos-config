eaf8d68b74bb794080a8d59829aa120f39aed53b
psiri <paulsiri1@gmail.com>
2024-01-24 08:39:39 -0800

    again

--- home/obs-studio/default.nix
+++ home/obs-studio/default.nix
@@ -6,18 +6,20 @@
   user,
   ...
 }: {
-  home-manager.users.${user}.programs.obs-studio = {
-    enable = true;
-    plugins = [ # List of plugins to install
+  home-manager.users.${user} = {
+    programs.obs-studio = {
+      enable = true;
+      plugins = [ # List of plugins to install
 
-    ];
-  };
-  home-manager.users.${user}.home.file.".config/hypr/per-app/obs-studio.conf" = {
-    text = ''
-      # windowrulev2 = opacity 0.8 0.8, class:^(obs-studio)$
-      # windowrulev2 = size 700 300, class:^(obs-studio)$
-      # windowrulev2 = tile, class:^(obs-studio)$
-      bind = $mainMod, O, exec, obs
-    '';
+      ];
+    };
+    home.file.".config/hypr/per-app/obs-studio.conf" = {
+      text = ''
+        # windowrulev2 = opacity 0.8 0.8, class:^(obs-studio)$
+        # windowrulev2 = size 700 300, class:^(obs-studio)$
+        # windowrulev2 = tile, class:^(obs-studio)$
+        bind = $mainMod, O, exec, obs
+      '';
+    };
   };
 }
