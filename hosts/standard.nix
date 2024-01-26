668b92a42e181f8aeb90fd444aa554e16959a1c0
psiri <paulsiri1@gmail.com>
2024-01-26 13:34:40 -0800

    qtbase

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -238,7 +238,7 @@
       python311Packages.boto3
       python311Packages.pip
       python311Packages.xmltodict
-      qt6.base
+      qtbase
       qt6.qtwayland # SecureCRT dependency
       ssm-session-manager-plugin # AWS Systems Manager Session Manager plugin
       swayidle
