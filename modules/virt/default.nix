0b41b6615f4748ef2d17dad1fdb8dd38ac978df4
Paul Siri <paulsiri1@gmail.com>
2024-03-27 11:25:50 -0700

    [3-27-24] - [0.0.2] - Validated virtualization support (#2)
    * prepare to test virtualization
    
    * try new method
    
    * user
    
    * try with gnome
    
    * add qemu customization
    
    * retry
    
    * fix
    
    * add kernel modules
    
    * update readme
    
    * readme update
    
    * re-pin rebuild to main

---
+++ modules/virt/default.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  pkgs,
+  lib,
+  user,
+  ...
+}: {
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      onBoot = "start";
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        swtpm.package = pkgs.swtpm;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  users.users.${user}.extraGroups = [ "libvirtd" ];
+
+  environment.systemPackages = with pkgs; [
+    libguestfs
+    # qemu_kvm # Redundant, captured in virtualisation
+    spice
+    spice-gtk
+    spice-protocol
+    virt-manager
+    virt-viewer
+    virtio-win
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
+  programs.virt-manager.enable = true;
+
+  home-manager.users.${user} = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+
+
+  systemd.services."libvertd".reloadIfChanged = true;
+}
