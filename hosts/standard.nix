6cb17d6100980b6f72798a7d54ba699f1c1bd445
psiri <paulsiri1@gmail.com>
2024-04-08 16:38:36 -0700

    fix

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -166,6 +166,7 @@
         #input-leap
         joplin-desktop
         kitty
+        okta-aws-cli
         # openconnect     # Open-source multi-VPN client supporting Cisco Anyconnect, Pulse Secure, GlobalProtect, etc
         # opensnitch      # Open-source application firewall
         remmina           # Open-source remote desktop client
