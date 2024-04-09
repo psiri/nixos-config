0b19ead4e5db6d002e3dd76c82c2cb8daa2e29dc
psiri <paulsiri1@gmail.com>
2024-04-08 19:10:10 -0700

    again

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,11 @@
-# import all nix files in the current folder,
-# and execute them with args as parameters
-# The return value is a list of all execution results, 
-# which is the list of overlays
-
-# args:
-# # execute and import all overlay files in the current
-# # directory with the given args
-# builtins.map
-#   # execute and import the overlay file
-#   (f: (import (./. + "/${f}") args))
-#   # find all overlay files in the current directory
-#   (builtins.filter
-#     (f: f != "default.nix")
-#     (builtins.attrNames (builtins.readDir ./.)))
+# This file defines overlays
+{inputs, ...}: {
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+}
