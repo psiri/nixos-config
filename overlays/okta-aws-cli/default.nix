3057cbc0c87f585138ce73ded5c5897c032a96dd
psiri <paulsiri1@gmail.com>
2024-04-08 18:25:25 -0700

    restore

--- overlays/okta-aws-cli/default.nix
+++ overlays/okta-aws-cli/default.nix
@@ -3,13 +3,13 @@
   # Customized okta-aws-cli package - override the source to use the unstable version
   okta-aws-cli = super.okta-aws-cli.overrideAttrs (old: {
     version = "2.1.2";
-    #subPackages = [ "cmd/okta-aws-cli" ];
+    subPackages = [ "cmd/okta-aws-cli" ];
     src = super.fetchFromGitHub {
       owner = "okta";
       repo = "okta-aws-cli";
       rev = "v2.1.2";
       sha256 = "sha256-MNaoCefJwUPWYPZ+AtQUHhm1ZKSFq+hCGGAFwBxrbWI=";
-      fetchSubmodules = false;
+      fetchSubmodules = true;
     };
 
     vendorHash = "sha256-SjABVO6tHYRc/1pYjOqfZP+NfnK1/WnAcY5NQ4hMssE=";
