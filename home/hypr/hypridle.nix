200d501dabf1a2638d7dbac659144d7c30c2136b
psiri <paulsiri1@gmail.com>
2024-04-12 08:44:44 -0700

    update timings

--- home/hypr/hypridle.nix
+++ home/hypr/hypridle.nix
@@ -6,9 +6,9 @@
   home-manager.users.${user}.home.file.".config/hypr/hypridle.conf" = {
     text = ''
 general {
-    lock_cmd = pidof hyprlock || hyprlock       # avoid starting multiple hyprlock instances.
-    before_sleep_cmd = loginctl lock-session    # lock before suspend.
-    after_sleep_cmd = hyprctl dispatch dpms on  # to avoid having to press a key twice to turn on the display.
+  lock_cmd = pidof hyprlock || hyprlock       # avoid starting multiple hyprlock instances.
+  before_sleep_cmd = loginctl lock-session    # lock before suspend.
+  after_sleep_cmd = hyprctl dispatch dpms on  # to avoid having to press a key twice to turn on the display.
 }
 
 # listener {
@@ -19,20 +19,20 @@ general {
 
 # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
 listener { 
-    timeout = 300                                          # 5min.
-    on-timeout = brightnessctl -sd rgb:kbd_backlight set 0 # turn off keyboard backlight.
-    on-resume = brightnessctl -rd rgb:kbd_backlight        # turn on keyboard backlight.
+  timeout = 300                                          # 5min.
+  on-timeout = brightnessctl -sd rgb:kbd_backlight set 0 # turn off keyboard backlight.
+  on-resume = brightnessctl -rd rgb:kbd_backlight        # turn on keyboard backlight.
 }
 
 listener {
-    timeout = 30                                 # 10min
-    on-timeout = loginctl lock-session            # lock screen when timeout has passed
+  timeout = 600                                 # 10min
+  on-timeout = loginctl lock-session            # lock screen when timeout has passed
 }
 
 listener {
-    timeout = 60                                 # 10.5min
-    on-timeout = hyprctl dispatch dpms off        # screen off when timeout has passed
-    on-resume = hyprctl dispatch dpms on          # screen on when activity is detected after timeout has fired.
+  timeout = 630                                 # 10.5min
+  on-timeout = hyprctl dispatch dpms off        # screen off when timeout has passed
+  on-resume = hyprctl dispatch dpms on          # screen on when activity is detected after timeout has fired.
 }
 
 # listener {
