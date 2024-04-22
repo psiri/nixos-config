ce7e95fcc8e47791771c93c4ff1ccc2ede68d300
psiri <paulsiri1@gmail.com>
2024-04-22 13:48:42 -0700

    fdsgsdfg

--- pkgs/pan-python/default.nix
+++ pkgs/pan-python/default.nix
@@ -3,6 +3,6 @@
 ( let
     pan-python = pkgs.callPackage ./derivation.nix { pkgs=pkgs; buildPythonPackage=pkgs.python311Packages.buildPythonPackage; setuptools=pkgs.python311Packages.setuptools; wheel=pkgs.python311Packages.wheel; };
   in pkgs.python311.buildEnv.override rec {
-    extraLibs = [ pan-python ];
+    extraLibs = [ pkgs.python311Packages.setuptools pkgs.python311Packages.wheel pan-python ];
 }
 ).env
