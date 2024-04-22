b31d9ac670f3ac2e3ce8815581a3a0ad8cb2f502
psiri <paulsiri1@gmail.com>
2024-04-22 09:10:23 -0700

    again

--- home/python/default.nix
+++ home/python/default.nix
@@ -1,97 +1,59 @@
 {
-  description = "Python Packages Example";
+  config,
+  inputs,
+  outputs,
+  pkgs,
+  user,
+  ...
+}: 
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  let
+    # python = pkgs.python3.override {
+    #   self = python;
+    #   packageOverrides = python-self: python-super: {
+    #     redshift-connector = python-super.redshift-connector.overridePythonAttrs (oldAttrs: {
+    #       # redshift-connector tests don't work
+    #       doCheck = false;
+    #     });
+    #     google-cloud-bigquery = python-super.google-cloud-bigquery.overridePythonAttrs (oldAttrs: {
+    #       # this package marked as broken, so normally we need run NIXPKGS_ALLOW_BROKEN=1; nix develop --impure
+    #       # however, we can override that here, because we like to live dangerously
+    #       meta = oldAttrs.meta // { broken = false; };
+    #       doCheck = false;
+    #       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ python-super.packaging ];
+    #     });
+    #   };
+    # };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+    pan-python = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "pan-python";
+      version = "0.25.0";
 
-          # python = pkgs.python3.override {
-          #   self = python;
-          #   packageOverrides = python-self: python-super: {
-          #     redshift-connector = python-super.redshift-connector.overridePythonAttrs (oldAttrs: {
-          #       # redshift-connector tests don't work
-          #       doCheck = false;
-          #     });
-          #     google-cloud-bigquery = python-super.google-cloud-bigquery.overridePythonAttrs (oldAttrs: {
-          #       # this package marked as broken, so normally we need run NIXPKGS_ALLOW_BROKEN=1; nix develop --impure
-          #       # however, we can override that here, because we like to live dangerously
-          #       meta = oldAttrs.meta // { broken = false; };
-          #       doCheck = false;
-          #       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ python-super.packaging ];
-          #     });
-          #   };
-          # };
+      src = pkgs.fetchFromGitHub {
+        owner = "kevinsteves";
+        repo = "pan-python";
+        rev = "0.25.0";
+        #sha256 = "OAVHx/a4uupVGXSWN2/3uem9/4i+TUkzTX4kp0uLY44=";
+      };
+    };
 
-          # pandas-gbq and db-types are not in nixpkgs so we will define them here
-          # pandas-gbq = pkgs.python3Packages.buildPythonPackage rec {
-          #   pname = "pandas-gbq";
-          #   version = "0.19.2";
+  in
+  with pkgs;
+  {
+    devShells.default = mkShell {
+      name = "default python";
+      packages = [
+        # put any non-Python packages here
+        # google-cloud-sdk
+        # Python packages:
+        (python.withPackages (p: with p; [
+          pan-python
+        ]))
+      ];
 
-          #   src = pkgs.fetchFromGitHub {
-          #     owner = "googleapis";
-          #     repo = "python-bigquery-pandas";
-          #     rev = "v${version}";
-          #     sha256 = "USs9VCJWkgOY2XXMiCA1RO3Bw+C+SGblRHNnOZ0pWjs=";
-          #   };
-
-          #   doCheck = false; # check requires infrastructure
-          # };
-
-          pan-python = pkgs.python3Packages.buildPythonPackage rec {
-            pname = "pan-python";
-            version = "0.25.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "kevinsteves";
-              repo = "pan-python";
-              rev = "0.25.0";
-              #sha256 = "OAVHx/a4uupVGXSWN2/3uem9/4i+TUkzTX4kp0uLY44=";
-            };
-
-            #doCheck = false; # check requires pip
-          };
-        
-          # YAML extension module for including YAML files from within YAML files
-          # pyyaml-include = pkgs.python3Packages.buildPythonPackage rec {
-          #   pname = "pyyaml-include";
-          #   version = "1.3.1";
-          #   pyproject = true;
-          #   src = pkgs.python3Packages.fetchPypi {
-          #     inherit pname version;
-          #     hash = "sha256-TLO04bquLsJRgI/h6K7V09IGmcVBhkyOR+2GarLxUDk=";
-          #   };
-          #   nativeBuildInputs = [
-          #     pkgs.python3Packages.setuptools-scm
-          #   ];
-          # };
-
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            name = "default python";
-            packages = [
-              # put any non-Python packages here
-              # google-cloud-sdk
-              # Python packages:
-              (python.withPackages (p: with p; [
-                pan-python
-              ]))
-            ];
-
-            shellHook = ''
-            echo "Installed Pyhton Packages"
-            '';
-          };
-        }
-      );
-}
+      shellHook = ''
+      echo "Installed Pyhton Packages"
+      '';
+    };
+  }
+#}  
