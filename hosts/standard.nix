57d99c72af0bf0ce47378dae6ad5104abf08c741
Paul Siri <paulsiri1@gmail.com>
2024-04-26 10:59:05 -0700

    Update standard.nix

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -158,7 +158,7 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel" "networkmanager" "docker" "libvirtd"];
+      extraGroups = ["wheel" "networkmanager" "docker" "libvirtd" "plugdev"];
       packages = with pkgs; [
         # discord
         # docker
