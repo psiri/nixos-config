2a6b3a4bb25a26ac58973073db2215e06f8f28a5
Paul Siri <paulsiri1@gmail.com>
2024-04-26 09:56:29 -0700

    Update per-device.nix

--- hosts/fw16-nix/per-device.nix
+++ hosts/fw16-nix/per-device.nix
@@ -1,7 +1,7 @@
 {user, pkgs, ...}: {
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
-      monitor=,3840x2160,auto,1
+      monitor=,2560x1600,auto,1
       monitor=DP-2,3840x2160,0x0,1
       # Rotate DP-3 and place it to the left of DP-2
       monitor=DP-3,3840x2160,-2160x0,1,transform,1
