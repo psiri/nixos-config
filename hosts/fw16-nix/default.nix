9e78946f233938eeda45a79b478452a9b7be9949
psiri <paulsiri1@gmail.com>
2024-04-25 19:26:19 -0700

    retry

--- hosts/fw16-nix/default.nix
+++ hosts/fw16-nix/default.nix
@@ -54,7 +54,7 @@ in
     sops.defaultSopsFile = ../secrets/secrets.yaml;
     sops.age.keyFile = "/nix/persist/home/${user}/.config/sops/age/keys.txt";
     sops.secrets."network_manager.env" = { };
-    sops.secrets.user_password.neededForUsers = true;
+    #sops.secrets.user_password.neededForUsers = true;
     #security.pam.services.${user}.enableKwallet = true;
 
 
