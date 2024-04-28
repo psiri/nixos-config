650dd57c4566a76483d2a967ba7791356ea389f8
psiri <paulsiri1@gmail.com>
2024-04-28 12:24:07 -0700

    refresh

--- hosts/fw16-nix/per-device.nix
+++ hosts/fw16-nix/per-device.nix
@@ -2,9 +2,9 @@
   home-manager.users.${user}.home.file.".config/hypr/per-device.conf" = {
     text = ''
       monitor=eDP-1,2560x1600@60.00,0x0,1
-      monitor=DP-2,3840x2160@144.00,auto,1
+      monitor=DP-2,3840x2160@143.98,auto,1
       # Rotate DP-3 and place it to the left of DP-2
-      monitor=DP-3,3840x2160@144.00,auto,1,transform,1
+      monitor=DP-3,3840x2160@60.00,auto,1,transform,1
 
       # trigger when the Lid Switch is turning on (lid closed)
       bindl = , switch:on:Lid Switch,exec,hyprctl keyword monitor "eDP-1, disable"
