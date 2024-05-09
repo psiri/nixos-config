18245101ab4a7bfb1756dc42254f1a3a353219fc
Paul Siri <paulsiri1@gmail.com>
2024-05-09 10:49:19 -0700

    add wg2

--- hosts/fw16-nix/network-manager.nix
+++ hosts/fw16-nix/network-manager.nix
@@ -13,7 +13,6 @@
   sops.templates."nm-wireguard-primary".content = ''
 [connection]
 id=BBG-WIREGUARD-PRIMARY
-uuid=b97fb374-db26-4192-812d-3b49f14fdfaf
 type=wireguard
 autoconnect=false
 interface-name=wg0
@@ -22,25 +21,25 @@ permissions=user:psiri:;
 [wireguard]
 mtu=1420
 peer-routes=false
-private-key=${config.sops.placeholder.fw16/wireguard_private_key_primary}
+private-key=${config.sops.placeholder.wireguard_private_key_primary}
 
-[wireguard-peer.${config.sops.placeholder.fw16/wireguard_peer_uuid_primary}]
-endpoint=${config.sops.placeholder.fw16/wireguard_peer_address_primary}:${config.sops.placeholder.fw16/wireguard_peer_port_primary}
-preshared-key=${config.sops.placeholder.fw16/wireguard_psk_primary}
+[wireguard-peer.${config.sops.placeholder.wireguard_peer_uuid_primary}]
+endpoint=${config.sops.placeholder.wireguard_peer_address_primary}:${config.sops.placeholder.wireguard_peer_port_primary}
+preshared-key=${config.sops.placeholder.wireguard_psk_primary}
 preshared-key-flags=0
 persistent-keepalive=25
-allowed-ips=${config.sops.placeholder.fw16/wireguard_allowed_ips_primary}
+allowed-ips=${config.sops.placeholder.wireguard_allowed_ips_primary}
 
 [ipv4]
-address1=${config.sops.placeholder.fw16/wireguard_ip_primary}
-dns=${config.sops.placeholder.fw16/wireguard_dns_primary}
-dns-search=${config.sops.placeholder.fw16/wireguard_search_domains_primary}
+address1=${config.sops.placeholder.wireguard_ip_primary}
+dns=${config.sops.placeholder.wireguard_dns_primary}
+dns-search=${config.sops.placeholder.wireguard_search_domains_primary}
 ignore-auto-dns=true
 ignore-auto-routes=true
 may-fail=false
 method=manual
 never-default=true
-${config.sops.placeholder.fw16/wireguard_allowed_routes_primary}
+${config.sops.placeholder.wireguard_routes_primary}
 
 [ipv6]
 addr-gen-mode=default
