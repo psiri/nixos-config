8455e6c68b74368a0bf2099f206d6f95ee0b3126
psiri <paulsiri1@gmail.com>
2024-07-24 05:13:37 -0700

    cleanup

--- hosts/standard.nix
+++ hosts/standard.nix
@@ -51,10 +51,8 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      # outputs.overlays.additions
-      # outputs.overlays.modifications
+
       #outputs.overlays.unstable-packages
-      
       outputs.overlays.pipewireOverlay
 
       # import from ../overlays files
