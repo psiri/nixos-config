533c5d062226828dbdd91dafd1245bf874033253
psiri <paulsiri1@gmail.com>
2024-09-07 11:46:59 -0700

    no patch

--- overlays/globalprotect/default.nix
+++ overlays/globalprotect/default.nix
@@ -17,11 +17,12 @@ in
       hash = "sha256-ip+BpBy6RxMgbu/OlMEEB2l42cTyxobR5FbCEz8uOz4=";
     };
 
-    #patchPhase = '' '';
-
     patchPhase = ''
-      substituteInPlace /globalprotect-openconnect-${version}/CMakeLists.txt \
-        --replace-quiet /etc/gpservice $out/etc/gpservice;
     '';
+
+    # patchPhase = ''
+    #   substituteInPlace /globalprotect-openconnect-${version}/CMakeLists.txt \
+    #     --replace-quiet /etc/gpservice $out/etc/gpservice;
+    # '';
   });
 }
