0d7486b4545807666c72d70167d2870ffd0ec8c2
psiri <paulsiri1@gmail.com>
2025-03-28 14:18:30 -0700

    fix hyprland windowrulev2 issues with kitty and pavucontrol

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -4,11 +4,11 @@
   user,
   ...
 }: {
-  home-manager.users.${user}.home.file.".config/hypr/per-app/audio.conf" = {
-    text = ''
-      windowrule = float, ^(pavucontrol)$
-    '';
-  };
+  # home-manager.users.${user}.home.file.".config/hypr/per-app/audio.conf" = {
+  #   text = ''
+  #     windowrule = float, pavucontrol
+  #   '';
+  # };
   users.users.${user}.packages = with pkgs; [pamixer pavucontrol];
   security.rtkit.enable = true;       # used by PulseAudio to acquire realtime priority
   #sound.enable = true;               # Not intended to be enabled with Pipewire. Disable to fix alsa-store.service failure on rebuild. See https://github.com/NixOS/nixpkgs/issues/319809
