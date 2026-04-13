c72a79f851483c63a8e30e99dc1e90ca233fc4f4
psiri <paulsiri1@gmail.com>
2026-04-13 05:52:02 -0700

    fix home-manager nixpkgs.config conflict

--- home/default.nix
+++ home/default.nix
@@ -22,23 +22,6 @@
       # inputs.ags.homeManagerModules.default # imports from root flake.nix then builds the package which is nice :)
     ];
 
-    nixpkgs = {
-      # You can add overlays here
-      # overlays = [
-      #   # Add overlays your own flake exports (from overlays and pkgs dir):
-      #   outputs.overlays.additions
-      #   outputs.overlays.modifications
-      #   outputs.overlays.unstable-packages
-      # ];
-      # Configure your nixpkgs instance
-      config = {
-        # Disable if you don't want unfree packages
-        allowUnfree = true;
-        # Workaround for https://github.com/nix-community/home-manager/issues/2942
-        allowUnfreePredicate = _: true;
-      };
-    };
-
     # Add stuff for your user as you see fit:
     # programs.neovim.enable = true;
     # programs.hyprland.enable = true; # Enable Hyprland window manager
